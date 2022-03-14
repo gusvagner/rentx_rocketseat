174f0bbb237d2a45becf699e033b7ad59ef92857
gustavo <gustavo@stallos.com.br>
2022-03-14 20:42:28 -0300

    dependency injection

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import multer from "multer";
-import createCategoryController from '../modules/cars/useCases/createCategory';
+import { CreateCategoryController } from '../modules/cars/useCases/createCategory/CreateCategoryController';
 import { importCategoryController } from '../modules/cars/useCases/importCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
@@ -9,10 +9,14 @@ const upload = multer({
   dest: "./tmp",
 });
 
-categoriesRoutes.post("/", (request, response) => {
-  console.log("aaa");
-  return createCategoryController().handle(request, response);
-});
+const createCategoryController = new CreateCategoryController();
+
+// categoriesRoutes.post("/", (request, response) => {
+//   console.log("aaa");
+//   return createCategoryController().handle(request, response);
+// });
+
+categoriesRoutes.post("/", createCategoryController.handle)
 
 categoriesRoutes.get("/", (request, response) => {
   return listCategoriesController.handle(request, response);
